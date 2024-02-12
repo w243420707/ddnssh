0bc73bd3c3ddd25f849b49fa57f8acc8f5bcb2d8
w243420707 <yooyu@msn.com>
2024-02-13 01:21:09 +0800

    Update xjp-cf-v4-ddns.sh

--- xjp-cf-v4-ddns.sh
+++ xjp-cf-v4-ddns.sh
@@ -37,7 +37,7 @@ CFUSER=yooyu@msn.com
 CFZONE_NAME=fly64jfgwhale.xyz
 
 # Hostname to update, eg: homeserver.example.com
-CFRECORD_NAME=xjpzz5.fly64jfgwhale.xyz
+CFRECORD_NAME=sg66ipq4hjobdnslbl.fly64jfgwhale.xyz
 
 # Record type, A(IPv4)|AAAA(IPv6), default IPv4
 CFRECORD_TYPE=A
